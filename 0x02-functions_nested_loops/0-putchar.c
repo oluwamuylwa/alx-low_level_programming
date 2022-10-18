79bd8163b3ae7b8af39df93c145f4ae794f9897c
oluwamuylwa <muyikjay@gmail.com>
2022-10-18 15:01:47 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - Entry program point
+* Description: A program that prints _putchar
+* followed by a new line.
+*
+* Return: Always 0(success).
+*/
+
+int main(void)
+{
+	char word[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
